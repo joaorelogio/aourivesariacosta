0c54a2dcc89410542d80a5773e713d3535c64bfd
João Paulo Relógio <joaoprelogio@gmail.com>
2018-10-24 18:07:41 +0100

    Working in qoute component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  quoteTxt: string;
+  quoteAuthor: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.quoteTxt = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis mi purus. Nunc justo turpis, commodo sit amet orci in, viverra scelerisque erat. Maecenas luctus vehicula purus, et lobortis tellus volutpat in. Phasellus ac ornare augue, eget varius leo. Proin fermentum, lorem ac viverra imperdiet, ipsum dolor condimentum lacus, vel ullamcorper odio tortor non arcu. Integer vitae orci varius, interdum dolor in, dictum lacus.';
+    this.quoteAuthor = 'Finibus Bonorum';
   }
 
 }
--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html'
 })
 export class QuoteComponent implements OnInit {
+
+  @Input() text: string;
+  @Input() author: string;
 
   constructor() { }
 
