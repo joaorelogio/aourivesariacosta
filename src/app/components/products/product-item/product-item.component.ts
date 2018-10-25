73b5689879343b6cb4b7cf58873478b203847fa6
João Paulo Relógio <joaoprelogio@gmail.com>
2018-10-25 22:50:44 +0100

    Create component product-item

---
+++ src/app/components/products/product-item/product-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../../services/product.service';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html'
+})
+export class ProductItemComponent implements OnInit {
+
+  @Input() info: any = {};
+
+  constructor(
+    protected router: Router,
+    protected route: ActivatedRoute,
+    public productService: ProductService
+  ) { }
+
+  ngOnInit() {
+    console.log(this.info);
+  }
+
+  openProductDetail(id: string) {
+    this.router.navigate(['product-detail', id], { relativeTo: this.route });
+  }
+
+}
