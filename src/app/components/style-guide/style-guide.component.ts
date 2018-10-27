00c0d6bdced27060fa41fed91ca11be39fd2dd51
João Paulo Relógio <joaoprelogio@gmail.com>
2018-10-28 00:03:58 +0100

    Creating style guide for forms

--- src/app/components/style-guide/style-guide.component.ts
+++ src/app/components/style-guide/style-guide.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-style-guide',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StyleGuideComponent implements OnInit {
 
-  constructor() { }
+  exampleForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  onReset() {
+    this.exampleForm.reset();
   }
 
 }
