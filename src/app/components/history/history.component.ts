f7a0c6fdb9f72bd6d9638cc63b82df5ce6effa72
João Paulo Relógio <joaoprelogio@gmail.com>
2018-10-28 00:15:04 +0100

    Working in history component

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -8,12 +8,20 @@ export class HistoryComponent implements OnInit {
 
   title: string;
   subtitle: string;
+  subtitle2: string;
+
+  quoteTxt: string;
+  quoteAuthor: string;
 
   constructor() { }
 
   ngOnInit() {
     this.title = 'a ourivesaria costa';
     this.subtitle = 'info';
+    this.subtitle2 = 'info 2';
+
+    this.quoteTxt = 'Porque gosto de me sentar à mesa e saber que estou à apoiar a produção nacional. Gosto de ver a minha terra a ser trabalhada por mãos portuguesas e proteger aquilo que é nosso. Por isso gosto de consumir o que sei que foi plantado, produzido, trabalhado, colhido pelos meus compatriotas, porque reconheco a qualidade do produto nacional e sei que ao apoiar o que é meu e teu estou a contribuir para mais e melhores momentos para Portugal.';
+    this.quoteAuthor = 'A Ourivesaria Costa - Uma paixão de gerações.';
   }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.quoteTxt = 'O saber e a arte são portugueses. As matérias primas também. Na tradição da melhor ourivesaria nacional, criamos preciosidades que vencem o tempo e prolongam a memória. São pequenos tesouros da nossa identidade, joías retiradas da cultura e do imaginário de um povo. Venha conhecê-las. Afinal, Portugal vale ouro.';
-    this.quoteAuthor = 'A Ourivesaria Costa';
+    this.quoteAuthor = 'A Ourivesaria Costa - O valor de ser português.';
   }
 
 }
