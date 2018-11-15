432da9cc9f32832c524ffa0cb03b01f486e89f39
João Paulo Relógio <joaoprelogio@gmail.com>
2018-11-15 00:00:58 +0000

    Appling loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public loading = false;
   title = 'aourivesariacosta';
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.Login();
+  }
+
+  Login() {
+    this.loading = true;
+  }
 }
