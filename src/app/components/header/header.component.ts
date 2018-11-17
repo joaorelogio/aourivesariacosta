ab659d77d55a703f78e27ff21fa356292b43e38c
João Paulo Relógio <joaoprelogio@gmail.com>
2018-11-17 00:17:42 +0000

    Working in header image logo loop

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,26 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+// import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy  {
+
+  showloader: Boolean = false;
+  subscription: Subscription;
+  timer: Observable<any>;
+  imgSrc: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.setTimer();
   }
 
-  initHoverLogo() {
-    let idx = 0;
-    fade();
-    function fade() {
-      if (idx >= $('#logotipo img').length) {
-        idx = 0;
-        $('#logotipo img').eq(idx).fadeIn(1500).delay(3000).fadeOut(1500, fade);
-        idx++;
-      }
+  ngOnDestroy() {
+    if ( this.subscription && this.subscription instanceof Subscription) {
+      this.subscription.unsubscribe();
     }
+  }
+
+  public setTimer() {
+    this.imgSrc = 'assets/logo/logo-fill-text.png';
+    const numbers = timer(5000, 5000);
+    numbers.subscribe(x => {
+      console.log(x);
+      if ( x === 5 ) {
+        this.imgSrc = 'assets/logo/logo-fill-text.png';
+        this.setTimer();
+      }
+    });
   }
 }
